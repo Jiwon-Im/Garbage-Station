8641f5ec1da14236f4141f113faaf3dcaa7eb6d7
Anggeumi <jia5642@naver.com>
2020-11-19 18:57:39 +0900

    register

--- app/src/main/java/com/example/gs/MainActivity.java
+++ app/src/main/java/com/example/gs/MainActivity.java
@@ -76,23 +76,20 @@ public class MainActivity extends AppCompatActivity implements NaverMap.OnMapCli
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
 
-    private FirebaseDatabase userDatabase;
-    private DatabaseReference userbaseReference;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.mContext = getApplicationContext();
 
-        init();
+        if(FirebaseAuth.getInstance().getCurrentUser() == null){
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+        }
 
         //파이어베이스 데이터베이스
         firebaseDatabase = FirebaseDatabase.getInstance(); //파이어베이스 데이터베이스 연동
         databaseReference = firebaseDatabase.getReference("gsbin"); //DB 테이블 연결
-
-        userDatabase = FirebaseDatabase.getInstance();
-        userbaseReference = userDatabase.getReference("userinfo");
 
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -116,7 +113,7 @@ public class MainActivity extends AppCompatActivity implements NaverMap.OnMapCli
         qrBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
+                Intent intent = new Intent(getApplicationContext(),QrActivity.class);
                 startActivity(intent);
             }
         });
@@ -126,8 +123,14 @@ public class MainActivity extends AppCompatActivity implements NaverMap.OnMapCli
         mBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
+                switch (v.getId()){
+                    case R.id.menuImg:
+                        FirebaseAuth.getInstance().signOut();
+                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+
             }
         });
 
@@ -277,32 +280,33 @@ public class MainActivity extends AppCompatActivity implements NaverMap.OnMapCli
         }
     }
 
-    private void init() {
-        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (firebaseUser == null) {
-            myStartActivity(SignUpActivity.class);
-        } else {
-            DocumentReference documentReference = FirebaseFirestore.getInstance().collection("users").document(firebaseUser.getUid());
-            documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                    if (task.isSuccessful()) {
-                        DocumentSnapshot document = task.getResult();
-                        if (document != null) {
-                            if (document.exists()) {
-                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                            } else {
-                                Log.d(TAG, "No such document");
-                                myStartActivity(CardRegisterActivity.class);
-                            }
-                        }
-                    } else {
-                        Log.d(TAG, "get failed with ", task.getException());
-                    }
-                }
-            });
-        }
-    }
+//    private void init() {
+//        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+//        if (firebaseUser == null) {
+//            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+//            startActivity(intent);
+//        } else {
+//            DocumentReference documentReference = FirebaseFirestore.getInstance().collection("users").document(firebaseUser.getUid());
+//            documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//                @Override
+//                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                    if (task.isSuccessful()) {
+//                        DocumentSnapshot document = task.getResult();
+//                        if (document != null) {
+//                            if (document.exists()) {
+//                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+//                            } else {
+//                                Log.d(TAG, "No such document");
+//                                myStartActivity(CardRegisterActivity.class);
+//                            }
+//                        }
+//                    } else {
+//                        Log.d(TAG, "get failed with ", task.getException());
+//                    }
+//                }
+//            });
+//        }
+//    }
 
     private void myStartActivity(Class c) {
         Intent intent = new Intent(this, c);
